752f2efb360df52b3f994f53857597c4c56ae3ce
MOCRUHA <gustavo.mocruha@gmail.com>
2024-04-10 21:19:56 -0300

    setting up gpc
    setting up gpc for google drive api

--- src/isbn_image_decoder.py
+++ src/isbn_image_decoder.py
@@ -1,17 +1,14 @@
 import cv2
 
-path = r"C:\Users\gusta\Pictures\barcode_00.jpg"
 
 def img_to_code(path):
 
-    img = cv2.imread(path)
+    img = cv2.imread(path) #read image from source
 
-    bd = cv2.barcode.BarcodeDetector()
+    bd = cv2.barcode.BarcodeDetector() #run de detector e decode isbn
 
-    decoded_info = bd.detectAndDecode(img)
+    decoded_info = bd.detectAndDecode(img) #save isbn decode as tuple
 
-    return decoded_info[0]
+    return decoded_info[0] #select only the isbn number
 
-isbn = img_to_code(path)
 
-print(isbn)
